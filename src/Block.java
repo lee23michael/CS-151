efdabb5211f11d19dd6837011a783c5ec2005eb9
David Lefaive <david.lefaive@sjsu.edu>
2019-03-11 13:38:21 -0700

    Updated Block and Position classes

--- src/Block.java
+++ src/Block.java
@@ -1,4 +1,41 @@
+import java.awt.Color;
 
+/**
+ * Represents blocks on a Grid.
+ */
 public class Block {
+	
+	/**
+	 * A constructor that creates a new block with Position p and Color c
+	 * 
+	 * @param p The Position of this Block.
+	 * @param c The Color of this Block.
+	 */
+	public Block(int x, int y, Color c ) {
+		this.p = new Position(x,y);
+		this.c = c;
+	}
+	/**
+	 * Sets the x and y position of this Block.
+	 */
+	public void setPostion(int x, int y) {
+		this.p = new Position(x,y);
+	}
+	/**
+	 * Accesses x coordinate of this Block.
+	 * @return Value for the x position of this Block.
+	 */
+	public int getXCoor() {
+		return this.p.getXPosition();
+	}
+	/**
+	 * Accesses y coordinate of this block.
+	 * @return Value for the y position of this Block.
+	 */
+	public int getYCoor() {
+		return this.p.getYPosition();
+	}
 
+	private Position p;
+	private Color c;
 }
--- src/Position.java
+++ src/Position.java
@@ -1,4 +1,43 @@
-
+/**
+ * Keeps track of the position of objects on a Grid.
+ */
 public class Position {
+/**
+ * Creates a new Position object with x and y coordinates.
+ * 
+ * @param x The x-coordinate
+ * @param y The y-coordinate
+ */
+	public Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	/**
+	 * Sets the position of this object with coordinates x and y.
+	 * 
+	 * @param x X coordinate
+	 * @param y Y coordinate
+	 */
+	public void setPosition(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	/**
+	 * Gets the x position of this object.
+	 * @return Value for the x position.
+	 */
+	public int getXPosition() {
+		return this.x;
+	}
+	/**
+	 * Gets the y position of this object.
+	 * @return Value for the y position.
+	 */
+	public int getYPosition() {
+		return this.y;
+	}
+	
+	private int x;
+	private int y;
 
 }
