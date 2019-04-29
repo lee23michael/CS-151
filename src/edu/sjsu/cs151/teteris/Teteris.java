85d28dcb04909c9c38e7c219a39d3fce0271fc48
vincent <vincent@192.168.137.103>
2019-04-29 01:38:37 -0700

    Timer function for controller, (not finished)

--- src/edu/sjsu/cs151/teteris/Teteris.java
+++ src/edu/sjsu/cs151/teteris/Teteris.java
@@ -1,7 +1,46 @@
 package edu.sjsu.cs151.teteris;
-//controller class
-public class Teteris {
+
+import java.util.Timer; 
+import java.util.TimerTask;
+
+import edu.sjsu.cs151.teteris.model.Model; 
+  
+
+public class Teteris 
+{ 
+	public String name = "ExampleName";
+	public Model model;
 	
+	public Teteris()
+	{
+		model = new Model(name);
+		
+	}
 	
+	public void start()
+	{
+		Timer timer = new Timer(); 
+        
+        timer.scheduleAtFixedRate(new TimerHelperDrop(), 300, 800);
+        
+        
+	}
+	
+    public static void main(String[] args) 
+    { 
+          
+        
+          
+    } 
+
+
+private class TimerHelperDrop extends TimerTask 
+{ 
+	public void run() 
+    { 
+        model.drop();
+    } 
+} 
 
 }
+  
