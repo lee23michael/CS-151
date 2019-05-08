956e44fff3740b8c306dd5ed60899406a131d53a
lee23michael <lee23michael@gmail.com>
2019-05-08 05:16:53 -0700

    Update test.java
    Test class updated

--- src/edu/sjsu/cs151/tetris/test.java
+++ src/edu/sjsu/cs151/tetris/test.java
@@ -3,25 +3,49 @@ import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import edu.sjsu.cs151.tetris.ui.Board;
 
 
 
 public class test {
 	
+	public static int game = 0;
+	
+	 private class loop extends TimerTask 
+	 { 
+	 	public void run() 
+	     { 
+	 		game = Teteris.getGame();
+	         
+	     } 
+	 } 
+	 
+	 public void startLoop()
+	 {
+		 Timer timer = new Timer(); 
+		  timer.scheduleAtFixedRate(new loop(), 1, 16);
+	 }
+	
 	 public static void main(String args[]) {
 		 
 		  Teteris test = new Teteris();
-	        
-	      test.start();
-		 
-
-		 
+		  test.startTitle();
+		  new test().startLoop();
 		  
+		  while(game == 0)
+		  {
+			  
+		  }
+		  System.out.println("run started");
 		  
+		  test.start();
+		
 		    }
+	 
+	
 		}
-
-
